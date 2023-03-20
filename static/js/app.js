27b65ef7fd6bc18a9e4cbaac593f2c29c58efcbc
Andrii M <andrey.meretsky@jetsoftpro.com>
2023-03-20 04:08:22 +0200

    refactor

--- static/js/app.js
+++ static/js/app.js
@@ -122,7 +122,7 @@ new Vue({
         },
         groupUpdate() {
             let g_id = this.$data.current_group.g_id
-            let json = JSON.stringify({"g_name": this.$data.g_name})
+            let json = JSON.stringify(this.$data.current_group)
             fetch("/groups/" + g_id, {
                 method: 'put',
                 headers: JSON_HEADERS,
